324759468e626203503d322db01556df7d97b694
lesand <leonelsd7@gmail.com>
2015-03-18 22:30:01 -0600

    adding model and controller of test database

--- API/index.js
+++ API/index.js
@@ -1,20 +1,45 @@
-var express = require("express"),
-app = express(),
-bodyParser  = require("body-parser"),
-methodOverride = require("method-override");
-mongoose = require('mongoose');
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
+
+// Connection to DB
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
+
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+// Import Models and controllers
+var models     = require('./models/tvshow')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
+
+// Example Route
 var router = express.Router();
-
 router.get('/', function(req, res) {
-   res.send("Hello World!");
+  res.send("Hello world!");
 });
-
 app.use(router);
 
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+// Start server
 app.listen(3000, function() {
-  console.log("first step to begin the project x");
+  console.log("cool project x");
 });
